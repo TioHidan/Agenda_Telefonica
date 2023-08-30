4a248d0a124797b80d5fbdae26ad7c65bdb8dfbb
Aluno Lab 135 <lab135@lab.senai>
2023-08-29 22:02:06 -0300

    Inserir um novo contato Version 1

---
+++ listen.py
@@ -0,0 +1,15 @@
+agendatelefonica=[]
+i=0
+NomeNum=[]
+
+nome =input('Digite o nome : ')
+numero=input('Digite o numero : ')
+NomeNum.insert(0,nome)
+NomeNum.insert(1,numero)
+print(NomeNum)   
+# for i in range (2):  
+#   agendatelefonica.append(NomeNum[i]) 
+#   print(agendatelefonica)
+
+# agendatelefonica.insert(NomeNum[2])
+# print(agendatelefonica)
